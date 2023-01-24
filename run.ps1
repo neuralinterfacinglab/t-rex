778b1f289af3d11a6c58c3992c48d25df1417122
jamigov <joaquin.amigov@gmail.com>
2023-01-24 16:22:39 +0100

    Add start scripts for Windows, Linux and macOS

--- run.ps1
+++
@@ -1,8 +0,0 @@
-conda activate trex
-
-$env:FLASK_ENV='development'
-$env:FLASK_APP='flaskr'
-
-start http://127.0.0.1:5000
-
-flask run
---
+++ run_macos.sh
@@ -0,0 +1,8 @@
+conda activate trex
+
+export FLASK_ENV=development
+export FLASK_APP=flaskr
+
+open http://127.0.0.1:5000
+
+flask run
