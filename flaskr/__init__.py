175fe43d9f08d62f1380104898725cd1534db553
jamigov <joaquin.amigov@gmail.com>
2023-01-24 17:30:41 +0100

    Fix logging

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,9 +1,10 @@
 # builtin
-import os
 import sys
 import logging
+from datetime import datetime
 
-# 3th party
+# 3rd party
+from pathlib import Path
 from flask import Flask
 from flask_cors import CORS
 import libs.utils as utils
@@ -12,45 +13,49 @@ import libs.utils as utils
 MAIN_DIR = utils.get_main_dir_from_config_file()
 PASSWORD = utils.get_pass_from_config_file()
 
-
 app = Flask(__name__)
 CORS(app)
 
 # define the format for the logs
-format = logging.Formatter('%(asctime)s %(levelname)s file:(%(filename)s) func:%(funcName)s(%(lineno)d) msg:%(message)s',datefmt='%d/%m/%Y %H:%M:%S')
+format = logging.Formatter(
+    '%(asctime)s %(levelname)s file:(%(filename)s (%(lineno)d)) %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
 
-# get default logger (werkzeug) and make it log to a file too
-web_logger = logging.getLogger('werkzeug')      # grabs underlying WSGI logger
-web_logger.setLevel(logging.WARNING)
-# TODO get the log file from the 'config.yaml'
-web_file_handler = logging.FileHandler('web_record.log') # creates handler for the log file
-web_file_handler.setFormatter(format)                # define the logging format for user defined messages
-web_file_handler.setLevel(logging.DEBUG)
+# define a file that will contain the default logs of flask and of our server
+date = datetime.now().strftime("%Y_%m_%d-%H_%M_%S")  # get the current date
+# define the name of the log file
+web_log_file_name = ''.join([date, '_web', '.log'])
+# define the name of the log file
+server_log_file_name = ''.join([date, '_server', '.log'])
+log_file_path = Path(MAIN_DIR)/'resources'/'logs'  # define the path
+log_file_path.mkdir(parents=True, exist_ok=True)  # create the log file
+
+# set the file handler for the web logger used by flask (werkzeug), and only log Warning messages
+web_logger = logging.getLogger('werkzeug')  # grabs underlying WSGI logger
+# remove the StreamHandler that werkzeug contains (we don't want this logger to print to the console)
+web_logger.handlers.clear()
+web_logger.setLevel(logging.DEBUG)
+web_file_handler = logging.FileHandler(
+    log_file_path/web_log_file_name)  # creates handler for the log file
 web_logger.addHandler(web_file_handler)
-
 
 # create a new logger to log the server messages
 server_logger = logging.getLogger('server')
 server_logger.setLevel(logging.DEBUG)
 
-fh = logging.FileHandler('server_record.log')
-fh.setLevel(logging.DEBUG)
+fh = logging.FileHandler(log_file_path/server_log_file_name)
 fh.setFormatter(format)
 
 stream = logging.StreamHandler(sys.stdout)
-stream.setLevel(logging.DEBUG)
 stream.setFormatter(format)
 
 server_logger.addHandler(fh)
 server_logger.addHandler(stream)
 
-server_logger.info("=================== NEW EXECUTION =================== ")
+server_logger.info("=================== NEW EXECUTION ===================")
 
-# get the views and the routes 
-from flaskr import views
+# load the views and routes
 from flaskr import routes
-
-
+from flaskr import views
 
 if __name__ == '__main__':
     server_logger.info('Starting server...')
